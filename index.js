a02a2acf50ea3c5c66bc43119e087771d67c67eb
Dang Nguyen Hai <dang682000@gmail.com>
2022-11-04 21:22:39 +0700

    wip: add new ui

---
+++ index.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $("#filter-btn").click(function () {
+    // $(".side-bar").style.display = "block";
+    console.log("oke");
+    $("#side-bar").toggleClass("display-side-bar");
+    // document.getElementsByClassName("side-bar").
+  });
+});
